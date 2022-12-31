15bd3345edc3c126103107a9c9c6aa0aa628bbec
SamuelLin213 <samuelddlin@gmail.com>
2022-12-31 14:34:47 -0800

    finish 3.2

---
+++ ch3/3.2.1.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+x_train = np.random.rand(10, 3)   # generate 10 random vectors of dimension 3
+x_test = np.random.rand(3)        # generate one more random vector of the same dimension
+
+def dist(a, b): # gets the Euclidean distance between 2 passed in lists
+    sum = 0
+    for ai, bi in zip(a, b):
+        sum = sum + (ai - bi)**2
+    return np.sqrt(sum)
+
+def nearest(x_train, x_test):
+    nearest = -1
+    min_distance = np.Inf
+    # add a loop here that goes through all the vectors in x_train and finds the one that
+    # is nearest to x_test. return the index (between 0, ..., len(x_train)-1) of the nearest
+    # neighbor
+    cnt = 0
+    for x in range(len(x_train)):
+        temp = dist(x_test, x_train[x])
+        cnt = cnt + 1
+        if temp < min_distance:
+            min_distance = temp
+            nearest = cnt
+    nearest = nearest-1
+    print(nearest)
+
+nearest(x_train, x_test)
+
